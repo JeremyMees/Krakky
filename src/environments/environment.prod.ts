e6fdf60acacbab578d25265864a2571c300515c0
JeremyMees <jeremymees123@gmail.com>
2022-03-01 16:51:33 +0100

    changed url in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SOCKET_ENDPOINT: 'ws://api.krakky.com/dashboard',
+  SOCKET_ENDPOINT: 'wss://api.krakky.com/dashboard',
   base_url: 'https://api.krakky.com',
 };
